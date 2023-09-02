d72d10dee352e6ce641b70ba29ffa3e7ebbd1194
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-09-02 14:10:14 -0300

    Customizando o tema default

--- App.tsx
+++ App.tsx
@@ -3,18 +3,19 @@ import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Loading />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
