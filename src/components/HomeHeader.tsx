41989dd2fa0ac63c70f0d6337d728a38f68bcab9
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-09-20 21:07:02 -0300

    Estilizando o HomeHeader

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -2,14 +2,14 @@ import { Heading, HStack, Text, VStack } from 'native-base';
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <VStack>
-        <Text color="gray.100">
+        <Text color="gray.100" fontSize="md">
           Olá,
         </Text>
 
-        <Heading color="gray.100">
-          Rodrigo
+        <Heading color="gray.100" fontSize="md">
+          Osvaldo
         </Heading>
       </VStack>
     </HStack>
