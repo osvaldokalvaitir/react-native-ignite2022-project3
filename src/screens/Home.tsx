d57c561ea96a90224f00321abd6fffc7d1434bfd
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-09-23 15:36:42 -0300

    Header da lista de exercícios

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { FlatList, HStack, VStack } from 'native-base';
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
@@ -32,6 +32,19 @@ export function Home() {
         maxH={10}
       />
 
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+
+        
+      </VStack>
     </VStack>
   );
 }
