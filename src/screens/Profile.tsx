afe69a88817c49960acae40382d1357ae8f92d2e
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-09-26 20:50:22 -0300

    Restringindo o tamanho da foto

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Alert, TouchableOpacity } from 'react-native';
 import { Center, ScrollView, VStack, Skeleton, Text, Heading } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
+import { FileInfo } from "expo-file-system";
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
@@ -31,8 +32,11 @@ export function Profile() {
       }
 
       if (photoSelected.assets[0].uri) {
-        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
-        console.log(photoInfo);
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri) as FileInfo;
+
+        if (photoInfo.size && (photoInfo.size  / 1024 / 1024 ) > 5){
+          return Alert.alert('Essa imagem é muito grande. Escolha uma de até 5MB.'); 
+        }
 
         setUserPhoto(photoSelected.assets[0].uri);
       }  
