1ddf8a2d11aa973ca03feb621160c7b6a6e29a08
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-09-26 20:09:57 -0300

    Ativando o carregamento da foto

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -15,18 +15,28 @@ export function Profile() {
   const [userPhoto, setUserPhoto] = useState('https://github.com/osvaldokalvaitir.png');
 
   async function handleUserPhotoSelected(){
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    });
+    setPhotoIsLoading(true);
 
-    if (photoSelected.canceled) {
-      return;
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+  
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      if (photoSelected.assets[0].uri) {
+        setUserPhoto(photoSelected.assets[0].uri);
+      }  
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false);
     }
-
-    setUserPhoto(photoSelected.assets[0].uri);
   }
 
   return (
