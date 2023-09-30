0133fa0c9e88051624bcef3bd989de1aec145070
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-09-30 14:38:21 -0300

    Adicionando regras aos input

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -55,6 +55,9 @@ export function SignUp() {
           <Controller 
             control={control}
             name="name"
+            rules={{
+              required: 'Informe o nome.'
+            }}
             render={({ field: { onChange, value } }) => (
               <Input 
                 placeholder="Nome"
