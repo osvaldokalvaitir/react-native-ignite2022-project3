d41982132a1ee59574218a8838744813e5842888
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2023-09-30 15:23:17 -0300

    Estilizando mensagens de validação

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -67,13 +67,10 @@ export function SignUp() {
                 placeholder="Nome"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.name?.message}
               />
             )}
           />
-
-          <Text color="white">
-            {errors.name?.message}
-          </Text>
 
           <Controller 
             control={control}
@@ -92,13 +89,10 @@ export function SignUp() {
                 autoCapitalize="none"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.email?.message}
               />
             )}
           />
-
-          <Text color="white">
-            {errors.email?.message}
-          </Text>
 
           <Controller 
             control={control}
